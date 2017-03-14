fe7fab81059f50005078f797c31ba78286e6de1c
nmandl001 <namanmandli@gmail.com>
2017-03-14 13:17:57 -0400

    Design pattern
    Buider Design pattern

---
+++ src/com/builderDesignPattern/CoffeeBuilder.java
@@ -0,0 +1,44 @@
+package com.builderDesignPattern;
+
+/* 
+ * CoffeeBuilder creates coffee by mixing the components[water,milk,sugar,coffee powder]. 
+ * CoffeeBuilder implements BeverageBuilder which is bluePrint for creating beverage objects.
+ * */
+public class CoffeeBuilder extends BeverageBuilder {
+
+	@Override
+	Beverage createBeverage() {
+		return new Beverage();
+	}
+
+	@Override
+	public void setWater() {
+		System.out.println("Step 1 : Boiling water");
+		getBeverage().setWater(40);
+	}
+
+	@Override
+	public void setMilk() {
+		System.out.println("Step 2 : Adding milk");
+		getBeverage().setMilk(50);
+	}
+
+	@Override
+	void setSugar() {
+		System.out.println("Step 3 : Adding sugar");
+		getBeverage().setSugar(10);
+	}
+
+	@Override
+	void setPowderQuantity() {
+		System.out.println("Step 4 : Adding 9 Grams of coffee powder");
+		getBeverage().setPowderQuantity(9);
+	}
+
+	@Override
+	void setBeverageType() {
+		System.out.println("Coffee");
+		getBeverage().setBeverageName("Coffee");
+	}
+
+}
---
+++ src/com/builderDesignPattern/TeaBuider.java
@@ -0,0 +1,43 @@
+package com.builderDesignPattern;
+
+/* 
+ * TeaBuilder creates Tea by mixing the components[water,milk,sugar,tea powder]. 
+ * It implements BeverageBuilder which is bluePrint for creating beverage objects.
+ * */
+public class TeaBuider extends BeverageBuilder {
+	@Override
+	Beverage createBeverage() {
+		return new Beverage();
+	}
+
+	@Override
+	public void setWater() {
+		System.out.println("Step 1 : Boiling water");
+		getBeverage().setWater(40);
+	}
+
+	@Override
+	public void setMilk() {
+		System.out.println("Step 2 : Adding milk");
+		getBeverage().setMilk(50);
+	}
+
+	@Override
+	void setSugar() {
+		System.out.println("Step 3 : Adding sugar");
+		getBeverage().setSugar(10);
+	}
+
+	@Override
+	void setPowderQuantity() {
+		System.out.println("Step 4 : Adding 9 Grams of tea powder");
+		getBeverage().setPowderQuantity(9);
+	}
+
+	@Override
+	void setBeverageType() {
+		System.out.println("Tea");
+		getBeverage().setBeverageName("Tea");
+	}
+
+}
