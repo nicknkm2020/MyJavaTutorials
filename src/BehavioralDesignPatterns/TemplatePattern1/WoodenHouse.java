0713f6e264e58d2b9e12305a664fc3f2dda23cb3
nmandl001 <namanmandli@gmail.com>
2017-03-14 20:22:49 -0400

    Template pattern
    Template pattern

---
+++ src/BehavioralDesignPatterns/TemplatePattern1/WoodenHouse.java
@@ -0,0 +1,30 @@
+package BehavioralDesignPatterns.TemplatePattern1;
+public class WoodenHouse extends HouseTemplate
+{
+
+  @Override
+  public void buildFoundation()
+  {
+    System.out.println("Building foundation with cement,iron rods,sand and wood");
+  }
+
+  @Override
+  public void buildPillars()
+  {
+    System.out.println("Building Pillars with Wood coating");
+  }
+
+  @Override
+  public void buildWalls()
+  {
+    System.out.println("Building Wooden Walls");
+  }
+
+  @Override
+  public void buildWindows()
+  {
+    System.out.println("Building Wooden Windows");
+
+  }
+
+}
